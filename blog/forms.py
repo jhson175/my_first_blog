0d350a5ac2743e984f1fd2d50d4bb4605a7b1131
JHSON <jhson175@naver.com>
2016-04-07 17:52:23 +0900

    Changed the HTML for the site and Adding CSS for the site

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
 class PostForm(forms.ModelForm): 
 
@@ -8,5 +8,9 @@ class PostForm(forms.ModelForm):
         model = Post   # 어떤 모델이 쓰이는지 장고에게 알려줌
         fields = ('title', 'text',)    # title과 text만 보여줌
         
+class CommentForm(forms.ModelForm):
 
+    class Meta:
+        model = Comment
+        fields = ('author', 'text',)
         
